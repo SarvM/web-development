da4d9c8b9aca5ab8c7d3d070fd6c5833bc099ccf
Saravana <saravana.m.architect@gmail.com>
2019-06-24 09:07:42 +0530

    Added bootstrap sample file

--- js/data-conversion.js
+++
@@ -1,9 +0,0 @@
-var answer = 42;
-answer = 'Thanks for all the fish...';
-
-x = 'The answer is ' + 42 
-y = 42 + ' is the answer' 
-
-
-'37' - 7 
-'37' + 7 
--- js/function-call.js
+++
@@ -1,8 +0,0 @@
-console.log(square(5));
-function square(n) { return n * n; }
-
-console.log(square); 
-console.log(square(5)); 
-var square = function(n) { 
-  return n * n; 
-}
